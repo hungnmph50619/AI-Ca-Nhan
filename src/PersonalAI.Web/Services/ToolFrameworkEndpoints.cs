30f15327febf6a633a16d1738be73710aae2e420
hungnmph50619 <hungnmph50619@gmail.com>
2026-09-18 17:20:13 +0700

    chore: nâng phiên bản khung công cụ lên 0.9.2

--- src/PersonalAI.Web/Services/ToolFrameworkEndpoints.cs
+++ src/PersonalAI.Web/Services/ToolFrameworkEndpoints.cs
@@ -4,7 +4,7 @@ namespace PersonalAI.Web.Services;
 
 public static class ToolFrameworkEndpoints
 {
-    public const string FrameworkVersion = "0.9.1";
+    public const string FrameworkVersion = "0.9.2";
 
     private static readonly string[] PermissionTypes =
     [

b0734b9e75fe25143bc2d9c8179a55402a047469
hungnmph50619 <hungnmph50619@gmail.com>
2026-09-18 18:41:55 +0700

    chore: align tool framework with v0.9.5

--- src/PersonalAI.Web/Services/ToolFrameworkEndpoints.cs
+++ src/PersonalAI.Web/Services/ToolFrameworkEndpoints.cs
@@ -4,7 +4,7 @@ namespace PersonalAI.Web.Services;
 
 public static class ToolFrameworkEndpoints
 {
-    public const string FrameworkVersion = "0.9.4";
+    public const string FrameworkVersion = "0.9.5";
 
     private static readonly string[] PermissionTypes =
     [

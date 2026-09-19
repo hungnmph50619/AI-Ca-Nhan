64174ab53a5501e19f361da30ea53e56c3e5b136
hungnmph50619 <hungnmph50619@gmail.com>
2026-09-19 13:03:32 +0700

    chore(v2.1.4): bump release version

--- src/PersonalAI.Web/Models/SystemModels.cs
+++ src/PersonalAI.Web/Models/SystemModels.cs
@@ -2,7 +2,7 @@ namespace PersonalAI.Web.Models;
 
 public static class PersonalAiRelease
 {
-    public const string Version = "2.1.3";
+    public const string Version = "2.1.4";
     public const string ApiContractVersion = "1";
     public const string Channel = "controlled";
 }
